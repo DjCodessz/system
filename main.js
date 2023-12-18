e9a287bf4b60a34f278769a5e5d9efd360101dbc
ATreeShine <66901031+ATreeShine@users.noreply.github.com>
2023-12-18 17:09:01 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+let numbers = [40, 100, 1, 5, 25, 10];
+numbers.sort(function(a, b) {
+  return a - b;
+});
+console.log(numbers); // Output: [1, 5, 10, 25, 40, 100]
+//this is for ascending number only
